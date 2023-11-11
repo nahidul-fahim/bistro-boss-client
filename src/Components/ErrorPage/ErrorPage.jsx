cbe735d3cda6ac658535be7941ea85bc6a7373f1
nahidul-fahim <nahidulfahim.official@gmail.com>
2023-11-11 20:25:25 +0600

    created react router environment added tailwind daisy and google font

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div className="h-[100vh] container mx-auto flex justify-center items-center p-5">
+            <h1 className="text-[200px] text-center font-bold">404</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
