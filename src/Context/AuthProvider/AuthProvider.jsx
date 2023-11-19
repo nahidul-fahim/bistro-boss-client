e6594ffa6d5f03618b67de96d5eb458751ca8592
nahidul-fahim <nahidulfahim.official@gmail.com>
2023-11-19 13:01:19 +0600

    added custom hook to get autprovider page info

--- src/Context/AuthProvider/AuthProvider.jsx
+++ src/Context/AuthProvider/AuthProvider.jsx
@@ -1,14 +1,58 @@
+import { createContext, useEffect, useState } from "react";
+import app from "../../Firebase/firebase.config";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 
-const AuthProvider = () => {
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
 
-    
 
+
+const AuthProvider = ({ children }) => {
+
+    const [currentUser, setCurrenUser] = useState('');
+    const [loading, setLoading] = useState(true);
+
+
+    // Create user using email-password
+    const createNewUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    // Login user with email-password
+    const LogInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // Logout the current user
+    const LogOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+
+
+    // Track the current user
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
+            setCurrenUser(user);
+            setLoading(false);
+            console.log("Current user:", user);
+        });
+        return () => unsubscribe();
+    }, [])
+
+
+
+    // send context value to the childrens
+    const authInfo = { currentUser, loading, createNewUser, LogInUser, LogOut };
 
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
